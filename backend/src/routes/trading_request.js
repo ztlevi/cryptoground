d7bfd4c39c8d9c78f31b469f3d50963f33c462ec
Ting Zhou <zhouting@umich.edu>
2018-03-03 13:23:34 -0800

    TODO test routing /trading_request

--- backend/src/routes/trading_request.js
+++ backend/src/routes/trading_request.js
@@ -6,7 +6,9 @@ var express = require('express');
 // var bodyParser = require('body-parser');
 var router = express.Router();
 
-var firebase = require('../dao/firebase.js');
+var firebase = require('../dao/firebase');
+import { cachedCryptoPrice } from '../dao/api';
+import { verifyTradingRequest } from '../utils/trading_verification';
 
 // router.use(bodyParser.json());
 router.post('/', function(req, res, next) {
@@ -17,10 +19,17 @@ router.post('/', function(req, res, next) {
     }
 
     firebase.fetchUserBalance(uid).then(response => {
-      if (!response) res.send('Fetching user balance failed!!!');
-      let verifyResult = verifyTradingRequest(req, balance);
+      if (!response) {
+        res.send('Fetching user balance failed!!!');
+        return;
+      }
+      let verifyResult = verifyTradingRequest(
+        req.body,
+        response,
+        cachedCryptoPrice
+      );
       if (verifyResult.status === 'complete') {
-        updateUserBalance(uid, verifyResult.balance).then(response => {
+        firebase.updateUserBalance(uid, verifyResult.balance).then(response => {
           if (!response)
             res.send("Cannot update user's new balance, request failed!");
           res.jsonp({
@@ -40,143 +49,7 @@ router.post('/', function(req, res, next) {
         });
       }
     });
-
-    // cachedCryptoPrice in api.js
-    // verifyTradingRequest(
-    //   fromCrypto,
-    //   toCrypto,
-    //   fromCryptoAmount,
-    //   toCryptoAmount,
-    // ).then(response => {
-    //   // approve => send back json result to user
-    //   // suspend => addToSuspendList
-    // });
   });
-  // console.log('Transaction requested');
-  // var re = {};
-  // var userTokenId = req.body.idToken;
-  // varify user token id
-  // if (firebase.verifyIdToken(userTokenId) == 0) {
-  // 	re['status'] = 400;
-  // 	re['message'] = 'Invalid user token id';
-  // 	res.jsonp(re);
-  // }
-  // else {
-  //   var uid = firbase.getUserId();
-  //   var tradingId = firebase.getTradingId();
-  // }
-  // // 0 for sell, 1 for buy
-  // var action = req.body.tradingType;
-  // var tradingFromSym = req.body.tradingFromSym;
-  // var tradingToSym = req.body.tradingToSym;
-  // var price = req.body.tradingPrice;
-  // var amount = req.body.tradingAmount;
-  // var timestamp = Date.now();
-  // var expiration = req.body.expiration;
-  // var pendingTrading = {
-  //   'uid': uid,
-  //   'tradingId': tradingId,
-  //   'tradingType': action,
-  //   'tradingFromSym': tradingFromSym,
-  //   'tradingToSym': tradingToSym,
-  //   'tradingPrice': price,
-  //   'tradingAmount': amount,
-  //   'timestamp': timestamp,
-  //   'expiration': exiration
-  // }
-  // // get marketPrice from firebase
-  // var marketPrice = 0;
-  // if (action == SELL) {
-  //   marketPrice = firebase.getMarcketPrice(tradingFromSym);
-  //   if (marketPrice <= price) {
-  //     // suspend
-  //     // adding to pending list
-  //     if (firebase.pushToSuspendList(pendingTrading)) {
-  //       re['status'] = 210;
-  //       re['message'] = 'Tansaction is pending';
-  //     }
-  //     else {
-  //       re['status'] = 500;
-  //       re['message'] = 'Failed';
-  //     }
-  //   }
-  //   else {
-  //     // success
-  //     // firebase operation
-  //     if (firebase.setTrading(uid, tradingId, {
-  //       'tradingType': action,
-  //       'tradingFromSym': tradingFromSym,
-  //       'tradingToSym': tradingToSym,
-  //       'tradingPrice': price,
-  //       'tradingAmount': amount
-  //     })) {
-  //       var userBalance = firebase.getUserInfo(uid, balance);
-  //       userBalance[tradingFromSym] -= amount;
-  //       userBalance[tradingToSym] += amount * price;
-  //       if (firebase.updateUser(uid, balance)) {
-  //         re['status'] = 200;
-  //         re['message'] = 'Success';
-  //       }
-  //       else {
-  //         firebase.removeTrading(uid, tradingId);
-  //         re['status'] = 500;
-  //         re['message'] = 'Failed';
-  //       }
-  //     }
-  //     else {
-  //       re['status'] = 500;
-  //       re['message'] = 'Failed';
-  //     }
-  //   }
-  // }
-  // else if (action == BUY) {
-  //   marketPrice = firebase.getMarcketPrice(tradingToSym);
-  //   if (marketPrice >= price) {
-  //     // suspend
-  //     // adding to pending list
-  //     if (firebase.pushToSuspendList(pendingTrading)) {
-  //       re['status'] = 210;
-  //       re['message'] = 'Tansaction is pending';
-  //     }
-  //     else {
-  //       re['status'] = 500;
-  //       re['message'] = 'Failed';
-  //     }
-  //   }
-  //   else {
-  //     // success
-  //     // firebase operation
-  //     if (firebase.setTrading(uid, tradingId, {
-  //       'tradingType': action,
-  //       'tradingFromSym': tradingFromSym,
-  //       'tradingToSym': tradingToSym,
-  //       'tradingPrice': price,
-  //       'tradingAmount': amount
-  //     })) {
-  //       var userBalance = firebase.getUserInfo(uid, balance);
-  //       userBalance[tradingFromSym] -= amount * price;
-  //       userBalance[tradingToSym] += amount;
-  //       if (firebase.updateUser(uid, balance, )) {
-  //         re['status'] = 200;
-  //         re['message'] = 'Success';
-  //       }
-  //       else {
-  //         firebase.removeTrading(uid, tradingId);
-  //         re['status'] = 500;
-  //         re['message'] = 'Failed';
-  //       }
-  //     }
-  //     else {
-  //       re['status'q] = 500;
-  //       re['message'] = 'Failed';
-  //     }
-  //   }
-  // }
-  // else {
-  //   re['status'] = 500;
-  //   re['message'] = 'Failed';
-  // }
-  // res.jsonp(re);
 });
 
 module.exports = router;
---
+++ backend/src/utils/trading_verification.js
@@ -0,0 +1,57 @@
+const TRADING_TYPE = { BUY: 'BUY', SELL: 'SELL' };
+
+// import { cachedCryptoPrice } from '../dao/api';
+
+// console.log(cachedCryptoPrice);
+
+export function verifyTradingRequest(
+  trading_request,
+  userBalance,
+  cachedCryptoPrice,
+) {
+  var from = trading_request.tradingFromSym;
+  var to = trading_request.tradingToSym;
+  var price = trading_request.tradingPrice;
+  var amount = trading_request.tradingAmount;
+  var action = trading_request.tradingType;
+  var re = {};
+
+  var marketPrice = 0;
+  var newBalance = userBalance;
+  if (action === TRADING_TYPE.SELL) {
+    marketPrice = cachedCryptoPrice[from];
+    if (marketPrice[to] <= price) {
+      // should suspend
+      re['status'] = 'suspend';
+    } else {
+      // success
+      newBalance[from] -= amount;
+      newBalance[to] += amount * marketPrice[to];
+      if (newBalance[from] < 0) {
+        re['status'] = 'failed';
+      } else {
+        re['status'] = 'complete';
+        re['balance'] = newBalance;
+      }
+    }
+  } else if (action === TRADING_TYPE.BUY) {
+    marketPrice = cachedCryptoPrice[to];
+    if (marketPrice[from] >= price) {
+      // suspend
+      re['status'] = 'suspend';
+    } else {
+      // success
+      newBalance[from] -= amount * marketPrice[from];
+      newBalance[to] += amount;
+      if (newBalance[from] < 0) {
+        re['status'] = 'failed';
+      } else {
+        re['status'] = 'complete';
+        re['balance'] = newBalance;
+      }
+    }
+  } else {
+    re['status'] = 'failed';
+  }
+  return re;
+}
