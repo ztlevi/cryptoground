c87fb28c3ac15895517a892f42ae277bec821f82
junzhi <junzhiwa@usc.edu>
2018-03-03 03:26:07 -0800

    fix bugs

--- frontend/src/hoc/Layout/Layout.js
+++ frontend/src/hoc/Layout/Layout.js
@@ -5,7 +5,7 @@ import classes from './Layout.css';
 import Navigation from '../../components/Navigation/Navigation';
 import Toolbar from '../../components/Toolbar/Toolbar';
 import Databoard from '../../components/Databoard/Databoard';
-import TradingForm from '../../components/TradingForm/TradingForm';
+//import TradingForm from '../../components/TradingForm/TradingForm';
 
 class Layout extends Component {
     
@@ -15,7 +15,7 @@ class Layout extends Component {
                 <Toolbar />
                 <Navigation />
                 <Databoard />
-                <TradingForm />
+                {/**<TradingForm />**/}
             </Aux>
        );
    }
